11d18a57530ac7eb0b4a16a0bc4d1d3f271cdab9
galialab <gidonle@post.bgu.ac.il>
2021-02-07 13:01:03 +0200

    version 0.0.9

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ copyright = '2020, Gidon Levakov'
 author = 'Gidon Levakov'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
